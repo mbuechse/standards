1cbfb7d31f49956b4bd25e249af6cbd56e23a108
Matthias Büchse <matthias.buechse@alasca.cloud>
2026-06-06 15:10:37 +0200

    Replace yaml scopes by regular standards
    Signed-off-by: Matthias Büchse <matthias.buechse@alasca.cloud>

--- Tests/chk_adrs.py
+++ Tests/chk_adrs.py
@@ -43,7 +43,7 @@ def iso_date(value):
 FRONT_MATTER_KEYS = {
     "type": ("Procedural", "Standard", "Decision Record").__contains__,
     "status": ("Proposal", "Draft", "Stable", "Deprecated", "Rejected").__contains__,
-    "track": ("Global", "IaaS", "KaaS", "IAM", "Ops").__contains__,
+    "track": ("Global", "IaaS", "KaaS", "IAM", "Ops", "Scopes").__contains__,
     "deprecated_at": optional(iso_date),
     "stabilized_at": optional(iso_date),
     "rejected_at": optional(iso_date),
